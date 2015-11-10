256aa0cc8007c9e281baa37676e86918be0e8633
Will <william.projects.2012@gmail.com>
2015-11-10 20:53:07 +0000

    Change app to app.src / add macro map for elixir projects

---
+++ src/erl8583_macro_map.erl
@@ -0,0 +1,12 @@
+-module(erl8583_macro_map).
+-compile(export_all).
+
+-include("erl8583_field_ids.hrl").
+
+%% for use in elixir projects
+
+card_acceptor_name_location() ->
+	?CARD_ACCEPTOR_NAME_LOCATION
+
+message_authentication_code() ->
+	?MESSAGE_AUTHENTICATION_CODE
